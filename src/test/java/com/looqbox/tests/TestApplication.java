3b5426e4e4ec4e2809b7fd27096f344d650ebbd2
maviifreitass <mariavitoria.freitas@yahoo.com>
2025-02-12 12:00:10 -0300

    chore: initial commit

---
+++ src/test/java/com/looqbox/tests/TestApplication.java
@@ -0,0 +1,22 @@
+package com.looqbox.tests;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+final class TestApplication {
+
+ 
+
+  @BeforeAll
+  static void start() {
+  
+  }
+
+  @Test
+  void test() {
+ 
+  }
+
+
+}
